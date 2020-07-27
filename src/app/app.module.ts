8e785c717283f070b2485ae29bdcead0c959b5f3
Joolanda <50619917+Joolanda@users.noreply.github.com>
2020-07-27 22:52:41 +0200

    defining structure of app model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,8 +9,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
-    // other imports ...
+    BrowserModule,
     HttpClientModule
   ],
   providers: [],
