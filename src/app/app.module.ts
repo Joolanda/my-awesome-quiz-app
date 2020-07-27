700a25a97710529bc815b93c567c03127fadce35
Joolanda <50619917+Joolanda@users.noreply.github.com>
2020-07-27 22:39:58 +0200

    inject the HttpClient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
@@ -9,6 +10,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule
+    // other imports ...
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
